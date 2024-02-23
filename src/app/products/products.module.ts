f426c117bf954a5fa6aa9897fc58362e354892ae
Julieta BACHUR <jbachur@CNP.com.ar>
2024-02-23 15:24:44 -0300

    listo

---
+++ src/app/products/products.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductsRoutingModule } from './products-routing.module';
+import { ProductComponent } from './pages/product/product.component';
+import { PriceComponent } from './components/price/price.component';
+// import { PriceComponent } from './components/price/price.component';
+
+
+@NgModule({
+  declarations: [
+    ProductComponent,
+    PriceComponent,
+    // PriceComponent
+  ],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule
+  ]
+})
+export class ProductsModule { }
